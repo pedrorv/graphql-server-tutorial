234630ef3dbb0f27282af6a795936560b4ded9ee
Pedro Reis <pedroreisv@gmail.com>
2017-08-06 09:40:48 -0300

    Add schema.js and setup expressGraphQL

---
+++ schema.js
@@ -0,0 +1,48 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = require('graphql')
+const axios = require('axios')
+
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/customers/${args.id}`)
+          .then(res => res.data)
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/customers`)
+          .then(res => res.data)
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const expressGraphQL = require('express-graphql')
+const schema = require('./schema')
 
 const app = express()
+const PORT = process.env.PORT || 8080
+
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema: schema,
+    graphiql: true
+  })
+)
 
 app.get('/', (req, res) => res.send('test'))
-
-const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
